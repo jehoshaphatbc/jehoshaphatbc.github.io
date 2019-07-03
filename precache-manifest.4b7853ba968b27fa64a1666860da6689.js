73f864089aa3ac5578f24663ad3c90775375aa93
Jehoshaphat BC <jehoshaphatbc@gmail.com>
2019-07-03 16:24:02 +0800

    Updates

---
+++ precache-manifest.4b7853ba968b27fa64a1666860da6689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c8c9146c8fe51a71f892237ea4c21c",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "5907237bf6ef344a5b23",
+    "url": "/react-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "917e9f640dd179305a23",
+    "url": "/react-pages/static/js/2.1dcc56b9.chunk.js"
+  },
+  {
+    "revision": "5907237bf6ef344a5b23",
+    "url": "/react-pages/static/js/main.66eef40b.chunk.js"
+  },
+  {
+    "revision": "1e067ef579591dacb6c4",
+    "url": "/react-pages/static/js/runtime~main.2afdb07a.js"
+  }
+]);
